87e31d9aa9d0b9f3bb6865aedd307a74b52f7b54
Jezreel Tan <jztan_25@yahoo.com>
2025-07-20 22:55:58 +0000

    More changes

--- basestation_node/lib/config.hpp
+++ basestation_node/lib/config.hpp
@@ -4,7 +4,7 @@
 	Faculty of Electrical and Computer Engineering
 	School of Engineering and Natural Sciences, University of Iceland
 
-	Title: Design and Implementation of a Low-Power LoRa Mesh Sensor Network 
+	Title: Design and Implementation of a Low-Power LoRa Mesh Sensor Network
 				 for Monitoring Soil Conditions on Icelandic Turf Roofs
 
 	Researcher: Jezreel Tan
@@ -33,17 +33,17 @@
 namespace config
 {
 	// WiFi Credentials
-	constexpr const char* SSID                       = "Vodafone-7zgp";
-	constexpr const char* PASSWORD                   = "yrdCy2x4";
-	// constexpr const char* SSID                       = "Turfroof Wifi";
-	// constexpr const char* PASSWORD                   = "turfroofhaskoliislands";
+	// constexpr const char* SSID                       = "Vodafone-7zgp";
+	// constexpr const char* PASSWORD                   = "yrdCy2x4";
+	constexpr const char* SSID                       = "Turfroof Wifi";
+	constexpr const char* PASSWORD                   = "turfroofhaskoliislands";
 
 	// Basestation HW_ID
 	constexpr uint8_t HW_ID                          = 8;
 
 	// ACTIVE_DEVICES: Indicates which HW_IDs are active
 	constexpr uint8_t ACTIVE_DEVICES[MAX_DEVICES] =
-	{ 
+	{
 		NOT_IN_USE, // HW_ID 0
 		ACTIVE, 	// HW_ID 1
 		NOT_IN_USE, // HW_ID 2
@@ -59,7 +59,7 @@ namespace config
 
 	// API Write Keys for ThingSpeak
 	constexpr const char* THINGSPEAK_API_KEYS[MAX_DEVICES] =
-	{ 
+	{
 		"RESERVED",                    // HW_ID 0
 		"H3DOMD0C33D9ARBO",            // HW_ID 1
 		"RESERVED",                    // HW_ID 2
